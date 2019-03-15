ba56f6c328aef8ca87f51190ef6bbca97ecbfb64
Optimistru <eugenykon@yandex.ru>
2019-03-15 13:51:56 +0400

    fix

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from requests import get, post, delete
 
-cookies = dict(session='eyJjc3JmX3Rva2VuIjoiYzJkYTQ2NGVkZmVkNDU4YzRkMTFkMmE4YmY5ODA0OTgxNDIxMmExYyIsInVzZXJfaWQiOjF9.XItzLw.eGQvt2a6_fRgafoQQF0XExBPZZs')
+cookies = dict(session='session cookies here')
 print(post('http://localhost:8000/api/v1/news',
            json={'title': 'Заголовок', 'content': 'Текст новости', 'user_id': 1}, cookies=cookies).json())
 print(get('http://localhost:8000/api/v1/news').json())
